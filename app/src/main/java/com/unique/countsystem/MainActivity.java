665ab29d60996fdad90bc7835139f6623d3160c2
SingleCycle <zhaoqiwang@hustunique>
2015-03-06 17:00:58 +0800

    first commit

--- app/src/main/java/com/unique/countsystem/MainActivity.java
+++ app/src/main/java/com/unique/countsystem/MainActivity.java
@@ -2,16 +2,44 @@ package com.unique.countsystem;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.InjectViews;
+import view.CustomViewPager;
 
 
 public class MainActivity extends ActionBarActivity {
+    @InjectView(R.id.view_page)
+    CustomViewPager mViewPager;
+    @InjectView(R.id.my_toolbar)
+    Toolbar mToolbar;
+    @InjectView(R.id.left_drawer)
+    ListView menuList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
+
+        setSupportActionBar(mToolbar);
+        mToolbar.setTitleTextColor(getResources().getColor(R.color.white));
+        mToolbar.setTitle(getTitle());
+
+        mToolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                // TODO Handle the menu item
+                return true;
+            }
+        });
+
     }
 
 
---
+++ app/src/main/java/util/DebugLog.java
@@ -0,0 +1,47 @@
+package util;
+
+import android.util.Log;
+
+import com.unique.countsystem.BuildConfig;
+
+
+public class DebugLog {
+    public static void i(String msg) {
+        if (isDebug()) {
+            Log.i(getCallerName(), msg);
+        }
+    }
+
+    public static void d(String msg) {
+        if (isDebug()) {
+            Log.d(getCallerName(), msg);
+        }
+    }
+
+    public static void v(String msg) {
+        if (isDebug()) {
+            Log.v(getCallerName(), msg);
+        }
+    }
+
+    public static void e(String msg) {
+        if (isDebug()) {
+            Log.e(getCallerName(), msg);
+        }
+    }
+
+    public static void w(String msg) {
+        if (isDebug()) {
+            Log.w(getCallerName(), msg);
+        }
+    }
+
+    private static String getCallerName() {
+        StackTraceElement[] elements = new Throwable().getStackTrace();
+        return elements[2].getClassName();
+    }
+
+    private static Boolean isDebug() {
+        return BuildConfig.DEBUG;
+    }
+}
