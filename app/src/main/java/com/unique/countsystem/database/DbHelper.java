37f3c7cd6bba4166c54aeb9acbb9ff0c20076c97
LionelCursor <danxionglei@hustunique.com>
2015-03-09 17:55:51 +0800

    conflict

--- app/src/main/java/com/unique/countsystem/database/DbHelper.java
+++ app/src/main/java/com/unique/countsystem/database/DbHelper.java
@@ -1,10 +1,13 @@
 package com.unique.countsystem.database;
 
 /**
+ * DatabaseHelper
+ * *****************
  * USER: ldx
  * EMAIL: danxionglei@foxmail.com
  * PROJECT_NAME: CountSystem
  * DATE: 2015/3/9
  */
 public class DbHelper {
+
 }
