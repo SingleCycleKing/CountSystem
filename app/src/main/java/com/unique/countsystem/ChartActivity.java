148b6e45f2ed1fcfca1fe751229ae64f5ee31310
androidshu <androidshu@qq.com>
2015-03-13 18:43:47 +0800

    overviewFragment git999

--- app/src/main/java/com/unique/countsystem/ChartActivity.java
+++ app/src/main/java/com/unique/countsystem/ChartActivity.java
@@ -1,19 +1,16 @@
 package com.unique.countsystem;
 
 import android.content.Intent;
-import android.support.v4.app.Fragment;
-import android.support.v7.app.ActionBar;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.ImageView;
 
-import com.unique.countsystem.R;
+import com.unique.countsystem.adapter.ChartAdapter;
 import com.unique.countsystem.fragment.PieChartFrag;
 import com.unique.countsystem.utils.BaseUtils;
+import com.unique.countsystem.view.DepthPageTransformer;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -22,6 +19,8 @@ public class ChartActivity extends ActionBarActivity {
 
     @InjectView(R.id.my_toolbar)
     Toolbar mToolbar;
+    private ChartAdapter chartAdapter;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,9 +34,16 @@ public class ChartActivity extends ActionBarActivity {
 
     private void init() {
 
-        getSupportFragmentManager().beginTransaction().
-                replace(R.id.chart_content, new PieChartFrag()).commit();
+        viewPager = (ViewPager) findViewById(R.id.view_pager) ;
 
+        getSupportFragmentManager().beginTransaction().
+                replace(R.id.view_pager, new PieChartFrag()).commit();
+
+        FragmentManager fm = getSupportFragmentManager();
+
+        chartAdapter = new ChartAdapter(fm);
+        viewPager.setPageTransformer(true,new DepthPageTransformer());
+        viewPager.setAdapter(chartAdapter);
     }
 
     @Override
