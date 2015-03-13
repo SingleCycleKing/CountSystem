ade803731a6a97c85cdde10678f0d5728781460f
androidshu <androidshu@qq.com>
2015-03-14 01:05:35 +0800

    overviewFragment git3333

--- app/src/main/java/com/unique/countsystem/fragment/PieChartFrag.java
+++ app/src/main/java/com/unique/countsystem/fragment/PieChartFrag.java
@@ -2,6 +2,7 @@ package com.unique.countsystem.fragment;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,18 +10,13 @@ import android.view.ViewGroup;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
-import com.github.mikephil.charting.utils.ColorTemplate;
 import com.github.mikephil.charting.utils.PercentFormatter;
 import com.unique.countsystem.R;
 
-import android.graphics.Typeface;
-import android.support.v4.app.Fragment;
+import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.PieChart;
-import com.github.mikephil.charting.components.Legend;
-import com.github.mikephil.charting.components.Legend.LegendPosition;
 import com.unique.countsystem.Record;
-import com.unique.countsystem.RecordTime;
 import com.unique.countsystem.database.DbHelper;
 import com.unique.countsystem.database.model.absenceType;
 
@@ -29,15 +25,11 @@ import java.util.List;
 
 public class PieChartFrag extends SimpleFragment {
 
-    public static Fragment newInstance() {
-        return new PieChartFrag();
-    }
-
+    private List<Record> records = DbHelper.getInstance().getAllAbsenceRecords();
     private PieChart mChart;
-
     protected String[] mParties = new String[]{
             "软工1班", "软工2班", "软工3班", "软工4班", "软工5班", "软工6班"
-            ,"数媒1班"
+            , "数媒1班"
     };
 
     @Override
@@ -64,39 +56,45 @@ public class PieChartFrag extends SimpleFragment {
         mChart.setRotationEnabled(true);
         mChart.setUsePercentValues(true);
         mChart.setTouchEnabled(true);
-        //mChart.setData(generatePieData());
         mChart.setCenterTextSize(20f);
-        setData(mParties.length) ;
-        mChart.getLegend().setEnabled(false);
-//        Legend l = mChart.getLegend();
-//        l.setPosition(LegendPosition.RIGHT_OF_CHART);
+
+        setData(mParties.length);
+
 
     }
 
-    private void setData(int count) {
+    private int setData(int count) {
 
-        float number = 100;
+        float number = getSum();
+        int[] numberClass = getNumberFromClass(mParties);
+        if (0 == number) {
+            Toast.makeText(getActivity(), "当前缺勤次数为0", Toast.LENGTH_SHORT).show();
+            return 0;
+        }
+
         ArrayList<Entry> yVals1 = new ArrayList<Entry>();
-        for (int i = 0; i < count ; i++) {
-            yVals1.add(new Entry((float) (Math.random() * number) + number / 5, i));
 
+        for (int i = 0; i < count; i++) {
+
+            yVals1.add(new Entry((numberClass[i] / (float) number), i));
         }
 
         ArrayList<String> xVals = new ArrayList<String>();
 
-        for (int i = 0; i < count ; i++) {
+        for (int i = 0; i < count; i++) {
 
             xVals.add(mParties[i % mParties.length]);
 
         }
+
         PieDataSet dataSet = new PieDataSet(yVals1, "");
         dataSet.setSliceSpace(3f);
 
         ArrayList<Integer> colors = new ArrayList<Integer>();
-        colors.add(Color.parseColor("#089d87")) ;
-        colors.add(Color.parseColor("#cf5348")) ;
-        colors.add(Color.parseColor("#fbcc8f")) ;
-        colors.add(Color.parseColor("#5d4037")) ;
+        colors.add(Color.parseColor("#089d87"));
+        colors.add(Color.parseColor("#cf5348"));
+        colors.add(Color.parseColor("#fbcc8f"));
+        colors.add(Color.parseColor("#475e88"));
 
         dataSet.setColors(colors);
 
@@ -105,17 +103,18 @@ public class PieChartFrag extends SimpleFragment {
         data.setValueTextSize(11f);
         data.setValueTextColor(Color.BLACK);
 
+        mChart.getLegend().setEnabled(false);
         mChart.setData(data);
         mChart.highlightValues(null);
-
         mChart.invalidate();
+        return 0;
     }
 
-    private List<Record> records = DbHelper.getInstance().getAllAbsenceRecords();
+
     private int getSum() {
-        int sum =0;
-        for (Record record:records){
-            if (record.getAbsenceType() != absenceType.NORMAL.toInteger()){
+        int sum = 0;
+        for (Record record : records) {
+            if (record.getAbsenceType() != absenceType.NORMAL.toInteger()) {
                 sum++;
             }
         }
@@ -123,12 +122,12 @@ public class PieChartFrag extends SimpleFragment {
     }
 
 
-    private int[] getNumberFromClass(String... _classes ){
+    private int[] getNumberFromClass(String... _classes) {
         int[] result = new int[_classes.length];
-        for (Record record:records){
+        for (Record record : records) {
             String a_class = record.getStudent().get_class();
-            for (int i = 0; i<_classes.length;i++){
-                if (_classes[i].equals(a_class)){
+            for (int i = 0; i < _classes.length; i++) {
+                if (_classes[i].equals(a_class)) {
                     result[i]++;
                 }
             }
